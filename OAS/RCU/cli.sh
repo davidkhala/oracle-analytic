11c7fcf7e23f896484295458f85b02a15df8f64c
Oracle Public Cloud User <opc@oracleanalyticserver.public.insecure.oraclevcn.com>
2023-01-03 15:47:35 +0000

    WIP

--- OAS/RCU/cli.sh
+++ OAS/RCU/cli.sh
@@ -1,7 +1,24 @@
-bin=/u01/app/Oracle/Middleware/Oracle_Home/oracle_common/bin/rcu -silent
+bin=/u01/app/Oracle/Middleware/Oracle_Home/oracle_common/bin/rcu
+
+edit=vi
+if code -v; then
+  edit=code
+fi
 
 configure() {
-  vi ~/.rcu/rcuResponseFile.properties
+  file=~/.rcu/rcuResponseFile.properties
+  if [ ! -f $file ]; then
+    $bin -silent -generateResponseFile # -silent is must
+  fi
+  $edit $file
+}
+create() {
+  # TODO WIP
+  echo $password | $bin -silent -createRepository -connectString $connectString -dbUser sys -dbRole sysdba \
+   -schemaPrefix ${schemaPrefix:-DEV} -component MDS -component STB\ 
+    $@
+}
+help() {
+  $bin -h
 }
 $@
-
